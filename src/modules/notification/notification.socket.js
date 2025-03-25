2c732b273ccb2327267eb0cd060233db092915aa
Indra Kurniawan <67799408+indrakuu@users.noreply.github.com>
2025-03-25 15:12:31 +0700

    Menambahkan room id untuk notifikasi

--- src/modules/notification/notification.socket.js
+++ src/modules/notification/notification.socket.js
@@ -1,9 +1,9 @@
 module.exports = (io, socket) => {
-    socket.on("notification_send", ({type}) => {
-        socket.emit("notification", { type });
-        console.log(`Notification type: ${type}`);
+    socket.on("notification_send", ({type, room}) => {
+        socket.emit("notification", { type, room });
+        console.log(`Notification type: ${type} has been sent to room ${room}`);
 
-        io.emit("notification_receive", { type });
-        console.log(`Notification has been received.`);
+        io.emit("notification_receive", { type, room });
+        console.log(`Notification has been received. Room: ${room}`);
     });
 };
